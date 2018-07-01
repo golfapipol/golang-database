fde849307b2c79388f93508651e64629c5c66691
Apipol Sukgler <apipol@computerlogy.com>
2018-07-01 19:21:39 +0700

    [Edited] แก้ไขใน main ให้ใช้ struct

--- src/main/main.go
+++ src/main/main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
-	"api"
+	apiLibrary "api"
 	"net/http"
 )
 
 func main() {
+	stubReadFunc := func(string) (apiLibrary.Sayhi, error) {
+		return apiLibrary.Sayhi{Id: 1, Description: "hello world"}, nil
+	}
+	api := apiLibrary.Api{
+		ReadFunc: stubReadFunc,
+	}
 	http.HandleFunc("/helloworld/read", api.ReadHandler)
 
 	http.ListenAndServe(":3000", nil)
